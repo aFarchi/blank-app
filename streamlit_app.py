7ad14a8141b04b4751e71f99b1aa326f3739f5f1
Alban Farchi <alban.farchi@enpc.fr>
2026-04-15 17:36:16 +0200

    changed to summary table

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,18 +7,14 @@ def get_string(space, name, token):
     site = Site(space=space, name=name)
     authenticator = Authenticator.from_token(token)
     content_manager = site.get_content_manager(authenticator=authenticator)
-    s = content_manager.download('test.csv')
+    s = content_manager.download('summary.csv')
     return pd.read_csv(io.StringIO(s.decode()))
 
-st.title("🎈 My new streamlit app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
-
+st.title("Experiment progress")
 space = st.secrets.sites.space
 name = st.secrets.sites.name
 token = st.secrets.sites.token
-st.write(f'loading string from {space}/{name}...')
+st.write(f'loading summary from {space}/{name}...')
 df = get_string(space, name, token)
 st.write('Done!')
 st.write(df)
